e06ce93d5d227b69f478638e7e20632ca2e3e075
mllangon <mllangon@myuax.com>
2023-12-05 11:26:22 +0100

    3 public 2

--- 3_Busqueda.cpp
+++ 3_Busqueda.cpp
@@ -13,3 +13,16 @@ public:
     Environment(const Environment&) = delete;
     Environment& operator=(const Environment&) = delete;
 
+    void setVariable(const std::string& name, const Value& value) {
+        symbols[name] = value;
+    }
+
+    std::optional<Value> lookup(const std::string& name) const {
+        auto it = symbols.find(name);
+        if (it != symbols.end()) {
+            return it->second;
+        } else {
+            return std::nullopt;
+        }
+    }
+
