98ae6d842449373f4f5d79e63ffcd80679b881ef
mllangon <mllangon@myuax.com>
2023-12-05 11:34:37 +0100

    4 public

---
+++ 4_Errores.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <variant>
+#include <stdexcept>
+
+class Environment {
+public:
+    using Value = std::variant<int, double, std::string>;
+
+    Environment() = default;
+
+    Environment(const Environment&) = delete;
+    Environment& operator=(const Environment&) = delete;
+
+
