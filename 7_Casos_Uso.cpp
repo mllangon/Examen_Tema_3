4958eb61f9d64db34c5b86e28a8a0ec321370df7
mllangon <mllangon@myuax.com>
2023-12-05 11:57:15 +0100

    7 public

---
+++ 7_Casos_Uso.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <variant>
+#include <stdexcept>
+
+class Environment {
+public:
+    using Value = std::variant<int, double, std::string>;
+
+    Environment() = default;
+
+    Environment(const Environment&) = delete;
+    Environment& operator=(const Environment&) = delete;
+
+    void setVariable(const std::string& name, const Value& value, bool readOnly = false) {
+        auto [iter, success] = symbols.insert({name, std::make_pair(value, readOnly)});
+        if (!success && iter->second.second) {
+            throw std::runtime_error("No se puede modificar la constante del juego '" + name + "'");
+        } else if (!success) {
+            iter->second.first = value;
+        }
+    }
+
