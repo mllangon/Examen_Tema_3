e263c7b8bbefab04846639dd2014c39864d631bd
mllangon <mllangon@myuax.com>
2023-12-05 11:39:16 +0100

    5 public 2

--- 5_Variant.cpp
+++ 5_Variant.cpp
@@ -19,3 +19,24 @@ public:
         std::cout << std::endl;
     }
 
+private:
+    Value value;
+};
+
+class Environment {
+public:
+
+    void addSymbol(const std::string& symbol, const Variant::Value& value) {
+        symbolTable[symbol] = value;
+    }
+
+    Variant::Value getSymbolValue(const std::string& symbol) const {
+        auto it = symbolTable.find(symbol);
+        if (it != symbolTable.end()) {
+            return it->second;
+        } else {
+            std::cerr << "Error: El simbolo '" << symbol << "' no esta definido." << std::endl;
+            return Variant::Value{};
+        }
+    }
+
