35903d6b18814d7d9e71f15503621861cc487d95
mllangon <mllangon@myuax.com>
2023-12-05 11:49:30 +0100

    6 public

---
+++ 6_Funciones.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <variant>
+#include <stdexcept>
+#include<vector>
+
+class Environment {
+public:
+    using Value = std::variant<int, double, std::string>;
+
+    Environment() = default;
+
+    Environment(const Environment&) = delete;
+    Environment& operator=(const Environment&) = delete;
+
+    void setVariable(const std::string& name, const Value& value, bool readOnly = false) {
+        auto [iter, success] = symbols.insert({name, std::make_pair(value, readOnly)});
+        if (!success && iter->second.second) {
+            throw std::runtime_error("No se puede modificar la constante del juego '" + name + "'");
+        } else if (!success) {
+            iter->second.first = value;
+        }
+    }
+
+    template<typename T>
+    T getVariableAs(const std::string& name) const {
+        auto it = symbols.find(name);
+        if (it == symbols.end()) {
+            throw std::runtime_error("La variable del juego '" + name + "' no existe");
+        }
+        try {
+            return std::get<T>(it->second.first);
+        } catch (const std::bad_variant_access&) {
+            throw std::runtime_error("Tipo incorrecto para la variable '" + name + "'");
+        }
+    }
+
+    void removeVariable(const std::string& name) {
+        auto it = symbols.find(name);
+        if (it != symbols.end()) {
+            symbols.erase(it);
+        }
+    }
